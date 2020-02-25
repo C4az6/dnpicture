facb77f0732348932290c2e7b16056598450ae9a
C4az6 <1026037632@qq.com>
2020-02-26 07:20:45 +0800

    uni-api使用演示

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -13,6 +13,24 @@ import { uniBadge } from "@dcloudio/uni-ui";
 export default {
   components: {
     uniBadge
+  },
+  onLoad() {
+    // http://157.122.54.189:9088/image/v3/homepage/vertical
+    // 原生微信小程序api语法
+    wx.request({
+      url: "http://157.122.54.189:9088/image/v3/homepage/vertical",
+      success: (res)=> {
+        console.log(res);
+      }
+    });
+
+    // 2 uni-api语法
+    uni.request({
+      url: "http://157.122.54.189:9088/image/v3/homepage/vertical",
+    })
+    .then(res=>{
+      console.log(res);
+    })
   }
 };
 </script>
