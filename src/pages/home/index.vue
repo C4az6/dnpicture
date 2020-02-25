19307a1c1cd1fadd9be70592a51238cc888e0fb3
C4az6 <1026037632@qq.com>
2020-02-26 07:09:34 +0800

    配置uni-ui

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,11 +1,20 @@
 <template>
   <view class="container">
-    <text class="iconfont iconvideocamera"></text>
+    <view class="iconfont iconvideocamera"></view>
+    <uni-badge text="1"></uni-badge>
+    <uni-badge text="2" type="success"></uni-badge>
+    <uni-badge text="3" type="primary" :inverted="true"></uni-badge>
   </view>
 </template>
 
 <script>
-export default {};
+// 引入uni-ui组件
+import { uniBadge } from "@dcloudio/uni-ui";
+export default {
+  components: {
+    uniBadge
+  }
+};
 </script>
 
 <style>
