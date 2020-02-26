41b7c8398d19fe9275358858cd480542e09572c3
C4az6 <1026037632@qq.com>
2020-02-27 02:27:48 +0800

    图片详情-特殊格式时间处理

--- src/pages/imgDetail/index.vue
+++ src/pages/imgDetail/index.vue
@@ -7,14 +7,14 @@
       </view>
       <view class="user-info">
         <view class="user-name">{{imgInfo.user.name}}</view>
-        <view class="user-time">{{imgInfo.atime}}</view>
+        <view class="user-time">{{imgInfo.cntime}}</view>
       </view>
     </view>
     <!-- 用户信息部分 end -->
 
     <!-- 图片部分 start -->
     <view class="img-bg">
-      <image :src="imgInfo.thumb+imgInfo.rule.replace('$<Height>',360)" mode="widthFix" />
+      <image :src="imgInfo.thumb2" mode="widthFix" />
     </view>
     <!-- 图片部分 end -->
 
@@ -28,6 +28,9 @@
 </template>
 
 <script>
+import moment from "moment";
+// 设置语言为中文
+moment.locale("zh-cn");
 export default {
   data() {
     return {
@@ -35,10 +38,10 @@ export default {
     };
   },
   onLoad() {
-    console.log(getApp().globalData);
     const { imgList, imgIndex } = getApp().globalData;
     this.imgInfo = imgList[imgIndex];
-    console.log(this.imgInfo);
+    this.imgInfo.thumb2 = this.imgInfo.thumb+this.imgInfo.rule.replace('$<Height>',360);
+    this.imgInfo.cntime = moment(this.imgInfo.atime*1000).fromNow();
   }
 };
 </script>
@@ -73,30 +76,24 @@ export default {
 
 .img-bg {
   image {
-
   }
 }
 
 .user-collect {
   display: flex;
-  align-items: center;
   height: 80rpx;
+  line-height: 80rpx;
   border-bottom: 3rpx solid #ccc;
   text.iconfont.icondianzan {
     flex: 1;
     display: flex;
     justify-content: center;
-    align-items: center;
-    
-  }
+}
 
   text.iconfont.iconshoucang {
     flex: 1;
     display: flex;
     justify-content: center;
-    align-items: center;
   }
 }
-
-
 </style>
