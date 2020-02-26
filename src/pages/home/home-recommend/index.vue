30e7887a5f98f2180f328aa9b4536597785e1fa3
C4az6 <1026037632@qq.com>
2020-02-26 09:02:53 +0800

    推荐模块-推荐列表完成

--- src/pages/home/home-recommend/index.vue
+++ src/pages/home/home-recommend/index.vue
@@ -1,10 +1,45 @@
 <template>
-  <view>推荐</view>
+  <view>
+    <view class="recommend-wrap">
+        <image :src="item.thumb" mode="widthFix" v-for="(item) in recommends" :key="item.id" />
+    </view>
+  </view>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      // 推荐列表数据
+      recommends: []
+    };
+  },
+  mounted() {
+    this.request({
+      url: "http://157.122.54.189:9088/image/v3/homepage/vertical",
+      data: {
+        // 要获取几条
+        limit: 30,
+        // 关键字
+        order: "hot",
+        // 要跳过几条
+        skip: 0
+      }
+    }).then(res => {
+      console.log(res.res);
+      this.recommends = res.res.homepage[1].items;
+    });
+  }
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+  .recommend-wrap {
+    display: flex;
+    flex-wrap: wrap;
+    >image {
+      width: 50%;
+      border: 5rpx solid #fff;
+    }
+  }
 </style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -66,13 +66,6 @@ export default {
       }
     }
   },
-  onLoad() {
-    this.request({
-      url: "http://157.122.54.189:9088/image/v3/homepage/vertical"
-    }).then(res => {
-      console.log(res);
-    });
-  }
 };
 </script>
 
