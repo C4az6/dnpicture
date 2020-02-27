7ad002f50d4ec2e891b2833c4d1a525b152af6ea
C4az6 <1026037632@qq.com>
2020-02-27 12:16:25 +0800

    专辑模块动态渲染完成

--- src/pages/imgDetail/index.vue
+++ src/pages/imgDetail/index.vue
@@ -24,6 +24,24 @@
       <text class="iconfont iconshoucang">收藏</text>
     </view>
     <!-- 点赞部分 end -->
+
+    <!-- 专辑信息 start -->
+    <view class="album-wrap">
+      <view class="album-title">相关</view>
+      <view class="album-list">
+        <view class="album-item" v-for="item in album" :key="item.id">
+          <view class="album-image">
+            <image :src="item.cover" mode="aspectFill"/>
+          </view>
+          <view class="album-info">
+            <view class="album-text">专辑</view>
+            <view class="album-name">{{item.name}}</view>
+            <text class="iconfont iconiconfontjiantou4"></text>
+          </view>
+        </view>
+      </view>
+    </view>
+    <!-- 专辑信息 end -->
   </view>
 </template>
 
@@ -34,14 +52,33 @@ moment.locale("zh-cn");
 export default {
   data() {
     return {
-      imgInfo: {}
+      imgInfo: {},
+      album: []
     };
   },
   onLoad() {
     const { imgList, imgIndex } = getApp().globalData;
     this.imgInfo = imgList[imgIndex];
-    this.imgInfo.thumb2 = this.imgInfo.thumb+this.imgInfo.rule.replace('$<Height>',360);
-    this.imgInfo.cntime = moment(this.imgInfo.atime*1000).fromNow();
+    this.imgInfo.thumb2 =
+      this.imgInfo.thumb + this.imgInfo.rule.replace("$<Height>", 360);
+    this.imgInfo.cntime = moment(this.imgInfo.atime * 1000).fromNow();
+
+    // 获取图片详情的id
+    this.getComments(this.imgInfo.id);
+  },
+  methods: {
+    getComments(id) {
+      this.request({
+        url: `http://157.122.54.189:9088/image/v2/wallpaper/wallpaper/${id}/comment`
+      }).then(data => {
+        this.album = data.res.album;
+        this.hot = data.res.hot;
+        this.comment = data.res.comment;
+        console.log(this.album);
+        console.log(this.hot);
+        console.log(this.comment);
+      });
+    }
   }
 };
 </script>
@@ -88,12 +125,61 @@ export default {
     flex: 1;
     display: flex;
     justify-content: center;
-}
+  }
 
   text.iconfont.iconshoucang {
     flex: 1;
     display: flex;
     justify-content: center;
+  }
+}
+
+.album-wrap {
+  padding: 10rpx;
+  .album-title {
+    padding: 10rpx 0;
+  }
+
+  .album-list {
+    .album-item {
+      display: flex;
+      padding: 10rpx 0;
+      border-bottom: 10rpx solid #eee;
+      .album-image {
+        flex: 1;
+        image {
+          width: 180rpx;
+          height: 180rpx;
+        }
+      }
+
+      .album-info {
+        flex: 3;
+        padding-left: 20rpx;
+        position: relative;
+        .album-text {
+          color: #fff;
+          background-color: $color;
+          padding: 20rpx;
+          font-size: 24rpx;
+          width: 90rpx;
+          height: 50rpx;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        .iconfont {
+          position: absolute;
+          top: 50%;
+          right: 10%;
+          transform: translateY(-50%);
+        }
+
+        .album-name {
+          color: #000;
+        }
+      }
+    }
   }
 }
 </style>
